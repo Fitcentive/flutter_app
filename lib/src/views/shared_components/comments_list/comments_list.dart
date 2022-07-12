b61fe9ec8e425b06d1c1f0e13db297ef7c79d461
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-07-12 12:04:55 -0400

    - Fix add comment UI

--- lib/src/views/shared_components/comments_list/comments_list.dart
+++ lib/src/views/shared_components/comments_list/comments_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
 import 'package:flutter_app/src/models/public_user_profile.dart';
 import 'package:flutter_app/src/models/social/social_post_comment.dart';
 import 'package:flutter_app/src/repos/rest/social_media_repository.dart';
@@ -51,6 +52,8 @@ class CommentsListViewState extends State<CommentsListView> {
 
   final TextEditingController _textEditingController = TextEditingController();
 
+  String? newUserComment;
+
   @override
   void dispose() {
     _textEditingController.dispose();
@@ -82,18 +85,21 @@ class CommentsListViewState extends State<CommentsListView> {
       body: BlocBuilder<CommentsListBloc, CommentsListState>(
         builder: (context, state) {
           if (state is CommentsLoaded) {
-            // todo - need to fix comments being hidden by keyboard
-            // todo - better UI and formatting for comment, more compact? Try ListTile
             return Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                SizedBox(
-                  height: 325,
+                LimitedBox(
+                  maxHeight: 350,
+                  // constraints: const BoxConstraints(maxHeight: 350),
                   child: _commentsListView(state.comments, state.userIdProfileMap),
                 ),
                 const Spacer(),
                 Align(
                   alignment: Alignment.bottomCenter,
-                  child: _createAddCommentView(),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(minHeight: 50, maxHeight: 75),
+                    child: _createAddCommentView(),
+                  ),
                 )
               ],
             );
@@ -108,55 +114,137 @@ class CommentsListViewState extends State<CommentsListView> {
     );
   }
 
+  _addCommentDialog() {
+    return GestureDetector(
+      onTap: () {
+        KeyboardUtils.hideKeyboard(context);
+      },
+      child: AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        content: ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 300),
+          child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              title: const Text("Add Comment", style: TextStyle(color: Colors.teal),),
+            ),
+            body: Align(
+              alignment: Alignment.bottomLeft,
+              child: TextField(
+                onChanged: (text) {
+                  setState(() {
+                    if (text.trim().isNotEmpty) {
+                      newUserComment = text;
+                    }
+                    else {
+                      newUserComment = null;
+                    }
+                  });
+                },
+                decoration: const InputDecoration(
+                    hintText: "Write a comment..."
+                ),
+                controller: _textEditingController,
+                expands: true,
+                minLines: null,
+                maxLines: null,
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    if (_textEditingController.value.text.trim().isEmpty) {
+                      newUserComment = null;
+                      _textEditingController.text = "";
+                    }
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")
+              ),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    _onSubmitButtonPressed();
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Post")
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   _createAddCommentView() {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxHeight: 100),
-      child: Card(
+    return Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
           ),
-          child: Container(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: ScreenUtils.getScreenWidth(context) * 0.75),
-                      child: IntrinsicHeight(
-                        child: TextField(
-                          controller: _textEditingController,
-                          decoration: const InputDecoration(
-                          hintText: "Write a comment...",
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: () {
+                  showDialog(context: context, builder: (context) {
+                    return _addCommentDialog();
+                  });
+                },
+                child: FittedBox(
+                  fit:BoxFit.fitHeight,
+                  child:  Container(
+                      width: ScreenUtils.getScreenWidth(context) * 0.75,
+                      padding: const EdgeInsets.all(15),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          newUserComment ?? "Write a comment...",
                         ),
-                      ),
-                    ),
+                      )
                   ),
-                  ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: ScreenUtils.getScreenWidth(context) * 0.25),
-                      child: IconButton(
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
                         onPressed: () {
-                          final currentAuthState = _authenticationBloc.state;
-                          if (currentAuthState is AuthSuccessUserUpdateState &&
-                              _textEditingController.value.text.isNotEmpty) {
-                            _commentsListBloc.add(
-                                AddNewComment(
-                                    postId: widget.postId!,
-                                    userId: currentAuthState.authenticatedUser.user.id,
-                                    comment: _textEditingController.value.text
-                                )
-                            );
-                            _textEditingController.text = "";
-                          }
+                          _onSubmitButtonPressed();
                         },
-                        icon: const Icon(Icons.send),
-                        color: Colors.teal,
+                        icon: Icon(
+                          Icons.send,
+                          color: newUserComment == null ? Colors.grey : Colors.teal,
+                        )
                     ),
                   )
-                ],
               )
-          )
+            ],
       ),
     );
+  }
+
+  _onSubmitButtonPressed() {
+    final currentAuthState = _authenticationBloc.state;
+    if (currentAuthState is AuthSuccessUserUpdateState && newUserComment != null && newUserComment!.isNotEmpty) {
+      _commentsListBloc.add(
+          AddNewComment(
+              postId: widget.postId!,
+              userId: currentAuthState.authenticatedUser.user.id,
+              comment: newUserComment!
+          )
+      );
+      _textEditingController.text = "";
+      setState(() {
+        newUserComment = null;
+      });
+    }
   }
 
   _commentsListView(List<SocialPostComment> comments, Map<String, PublicUserProfile> userProfiles) {
@@ -179,17 +267,20 @@ class CommentsListViewState extends State<CommentsListView> {
           onTap: () {
             KeyboardUtils.hideKeyboard(context);
           },
-          child: ListView.builder(
-            itemCount: comments.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index >= comments.length) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                final currentComment = comments[index];
-                final userProfile = userProfiles[currentComment.userId];
-                return _commentListItem(currentComment, userProfile);
-              }
-            },
+          child: Scrollbar(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: comments.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index >= comments.length) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  final currentComment = comments[index];
+                  final userProfile = userProfiles[currentComment.userId];
+                  return _commentListItem(currentComment, userProfile);
+                }
+              },
+            )
           ),
         ),
 
@@ -198,85 +289,55 @@ class CommentsListViewState extends State<CommentsListView> {
   }
 
   _commentListItem(SocialPostComment comment, PublicUserProfile? userProfile) {
-    return Container(
-      child: Card(
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
         child: Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: WidgetUtils.skipNulls(
-                [
-                  _userHeader(userProfile),
-                  WidgetUtils.spacer(10),
-                  _userCommentText(comment),
-                  _userCommentTime(comment),
-                ]
-            ),
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: ImageUtils.getUserProfileImage(userProfile, 100, 100),
           ),
         ),
       ),
-    );
-  }
-
-  _userHeader(PublicUserProfile? publicUser) {
-    return Row(
-      children: [
-        GestureDetector(
-          onTap: () {
-            Navigator.pushAndRemoveUntil(context, UserProfileView.route(publicUser!), (route) => true);
-          },
-          child: CircleAvatar(
-            radius: 30,
-            child: Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: ImageUtils.getUserProfileImage(publicUser, 100, 100),
-              ),
-            ),
-          ),
-        ),
-        WidgetUtils.spacer(20),
-        Text(
-          StringUtils.getUserNameFromUserId(publicUser),
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
+      title: Text(
+        StringUtils.getUserNameFromUserId(userProfile),
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: _userCommentText(comment),
     );
   }
 
   _userCommentText(SocialPostComment comment) {
-    return Row(
-      children: [
-        Expanded(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(2.5, 0, 0, 5),
+    return Container(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+      child:  Column(
+          children: [
+            Align(
+              alignment: Alignment.bottomLeft,
               child: Text(
                 comment.text,
                 style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                child: Text(
+                  DateFormat("hh:mm      yyyy-MM-dd").format(comment.createdAt),
+                  style: const TextStyle(
+                      fontSize: 10
+                  ),
                 ),
               ),
             )
-        )
-      ],
+          ],
+        ),
     );
   }
 
-  _userCommentTime(SocialPostComment comment) {
-    return Row(
-      children: [
-        Expanded(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(2.5, 0, 0, 0),
-              child: Text(
-                DateFormat("hh:mm      yyyy-MM-dd").format(comment.createdAt),
-              ),
-            )
-        )
-      ],
-    );
-  }
 }
