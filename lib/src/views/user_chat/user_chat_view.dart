3b550634e06d06f94cf1a0504b975a970ae2f635
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-08-08 18:09:34 -0400

    fix: Rename web app at start time - Was flutter_app until appName kicked in

--- lib/src/views/user_chat/user_chat_view.dart
+++ lib/src/views/user_chat/user_chat_view.dart
@@ -586,7 +586,6 @@ class UserChatViewState extends State<UserChatView> {
       types.TextMessage message,
       types.PreviewData previewData,
       ) {
-    print("PREVIEW DATA FETFCHED SON");
     // final index = _messages.indexWhere((element) => element.id == message.id);
     // final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
     //   previewData: previewData,
